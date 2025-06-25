bd49e02d2121de3e7772e525ebcf95730fb151e8
zedmatrix <zedmatrix007@gmail.com>
2025-06-25 09:54:35 -0700

    Create 1-mount-create-directories.sh

---
+++ stage1/1-mount-create-directories.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+LFS=${LFS:-/mnt/lfs}
+
+source check_fs.sh || { echo "Failed to source check_fs"; exit 1; }
+
+# Mount ROOT partition if not already mounted
+if ! mountpoint -q "$LFS"; then
+    echo "Mounting $ROOT to $LFS"
+    mount -v --mkdir "$ROOT" "$LFS" || {
+        echo "Failed to mount $ROOT to $LFS"
+        exit 1
+    }
+    chown -v root:root $LFS
+    chmod -v 755 $LFS
+else
+    echo "$LFS is already mounted."
+fi
+
+[[ -n $SWAP ]] && /sbin/swapon $SWAP || { echo "Failed to Activate Swap"; exit 1; }
+
+# Setup zbuild paths
+mkdir -pv $LFS/zbuild/{tmp,log}
+
+# Setup LFS Limited directories
+mkdir -pv $LFS/{etc,var,tools,sources} $LFS/usr/{bin,lib,sbin}
+
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
+
+case $(uname -m) in
+  x86_64) mkdir -pv $LFS/lib64 ;;
+esac
